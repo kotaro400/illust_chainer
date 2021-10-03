5c5abdd0a53d3141277fd035219e9ad2ebae003e
kotaro <ktrst0622@gmail.com>
2021-10-03 17:59:25 +0900

    メイン画面のヘッダー作成

---
+++ src/MainHeader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const headerStyle: React.CSSProperties = {
+  height: "60px",
+  backgroundColor: "#fff"
+};
+
+const MainHeader = () => {
+  return (
+    <header style={headerStyle}>
+    </header>
+  );
+};
+
+export default MainHeader;
--- src/Pictures.tsx
+++ src/Pictures.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import MainHeader from "./MainHeader";
 
 const Pictures = () => {
   return (
     <div>
+      <MainHeader />
       <Link to="/new">絵を追加する</Link>
     </div>
   );
