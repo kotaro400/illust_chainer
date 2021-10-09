3a7c2026b888cb9ff9803de7bc03815b1d5d61f1
kotaro <ktrst0622@gmail.com>
2021-10-09 13:31:43 +0900

    shareボタン配置

--- src/Result.tsx
+++ src/Result.tsx
@@ -2,9 +2,12 @@ import styled from "styled-components";
 import { BsCircle } from "react-icons/bs";
 import { ImCross } from "react-icons/im"
 import { useLocation } from "react-router-dom";
+import { TwitterShareButton, TwitterIcon, LineShareButton, LineIcon } from "react-share";
 import MainHeader from "./MainHeader";
 import PictureHistory from "./PictureHistory";
 import { Picture } from "./types";
+
+const url = process.env.REACT_APP_URL || "";
 
 const ResultComponent = styled.div`
   background-color: #f8f8fa;
@@ -21,6 +24,17 @@ const ResultComponent = styled.div`
     text-align: center;
     font-size: 150px;
   }
+  p {
+    font-size: 20px;
+    text-align: center;
+    font-weight: bold;
+    margin: 20px;
+  }
+`;
+
+const ShareWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
 `;
 
 const Result = () => {
@@ -28,6 +42,7 @@ const Result = () => {
 
   if (location.state) {
     const state = location.state as { pictures: Picture[], isChained: boolean };
+    const currentOrder = state.pictures.length;
     return (
       <ResultComponent>
         <MainHeader />
@@ -36,6 +51,15 @@ const Result = () => {
             <>      
               <h1>正解</h1>
               <BsCircle className="icon" />
+              <p>次の絵を描いてもらおう!</p>
+              <ShareWrapper>
+                <TwitterShareButton url={url} title={`現在${currentOrder}連続成功！`}>
+                  <TwitterIcon round={true} />
+                </TwitterShareButton>
+                <LineShareButton url={url} title={`現在${currentOrder}連続成功！`}>
+                  <LineIcon round={true} />
+                </LineShareButton>
+              </ShareWrapper>
             </>
           ) : (
             <>      
