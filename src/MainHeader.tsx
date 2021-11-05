817e23f2967ef11e25a581e3bfddc16872a1d6f3
kotaro <ktrst0622@gmail.com>
2021-11-05 14:04:07 +0900

    ロゴ変更

--- src/MainHeader.tsx
+++ src/MainHeader.tsx
@@ -5,12 +5,18 @@ const headerStyle: React.CSSProperties = {
   height: "60px",
   backgroundColor: "#fff",
   position: "relative",
+  textAlign: "center",
 };
 
 const MainHeader = () => {
   return (
     <header style={headerStyle}>
-      <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="" />
+      <img
+        src={`${process.env.PUBLIC_URL}/apple.png`}
+        width="60"
+        height="60"
+        alt=""
+      />
       <RuleModal />
     </header>
   );
